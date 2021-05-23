56ca6ea320d54cf8be90b6558ad03f4ddc4fbaa9
rsjeeva <75556646+rsjeeva@users.noreply.github.com>
2021-05-23 20:51:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,89 @@
-
+var cn = document.getElementById('content');
 var count=1;
 
-var count1=0;
-var nav = document.getElementById('nav');
-
-var div = document.createElement('div');
-
-var link1 = document.createElement('a');
-var link2 = document.createElement('a');
-var link3 = document.createElement('a');
-
-function display(){
-
-    if(count%2!==0)
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var c1 = document.getElementById('content1');
+var c2 = document.getElementById('c2');
+var c3 = document.getElementById('c3');
+var c4 = document.getElementById('c4');
+var c5 = document.getElementById('c5');
+var c6 = document.getElementById('c6');
+var c7 = document.getElementById('c7');
+var c8 = document.getElementById('c8');
+next.addEventListener('click', function()
+{
+    
+    if(count>=1 && count<8)
     {
-    link1.innerHTML='About';
-    link1.href='#About';
-    link1.setAttribute('class','hove');
-    link1.setAttribute('id','clic1');
-    link1.style.color='#9A9DA0';
-    link1.style.textDecoration='none';
-    link1.style.fontSize='1rem';
-    link1.style.marginLeft="2rem";
-    link1.style.paddingBottom='0.5rem';
-    div.append(link1);
-
-    link2.innerHTML='Services';
-    link2.href='#Services';
-    link2.setAttribute('class','hove');
-    link2.setAttribute('id','clic2');
-    link2.style.color='#9A9DA0';
-    link2.style.textDecoration='none';
-    link2.style.fontSize='1rem';
-    link2.style.marginLeft="2rem";
-    link2.style.paddingBottom='0.5rem';
-    div.append(link2);
-
-    link3.innerHTML='Contacts';
-    link3.setAttribute('class','hove');
-    link3.setAttribute('id','clic3');
-    link3.href='#Contact';
-    link3.style.color='#9A9DA0';
-    link3.style.textDecoration='none';
-    link3.style.fontSize='1rem';
-    link3.style.marginLeft="2rem";
-    link3.style.paddingBottom='0.5rem';
-    div.append(link3);
-
-    div.style.display="block";
-
-    // nav.append(div);
-
-    div.style.position="fixed";
-    // div.style.zIndex='10';
-    div.style.display='flex';
-    div.style.flexDirection='column';
-    div.style.width='100%';
-    div.style.height='15vh';
-    div.style.top='5.5rem';
-    div.style.justifyContent='space-between';
-    div.style.backgroundColor='#343A40';
-
-
-    document.body.append(div);  
     count++;
+    display(count);
+    // count++;
+    }
+    console.log(count);
+});
+
+prev.addEventListener('click',function(){
+    if(count>1 && count<=8)
+    {
+    count--;
+    display(count);
+    }
+    console.log('prev: ',count);
+});
+
+function display(x){
+    if(count===1)
+    {
+        cn.innerHTML='';
+        cn.append(c1);
     }
 
-    else{
-        console.log(count);
-        count++;
-
-        div.style.display='none';
-        
-        // link1.innerHTML='';
-        // link2.innerHTML='';
-        // link3.innerHTML='';
-        // div.append(link1,link2,link3);
-        // nav.append(div);
-        
+    else if(count===2)
+    {
+        c2.setAttribute('id', 'content2');
+        cn.innerHTML='';
+        cn.append(c2);
     }
-
-    var clik1 = document.getElementById('clic1');
-    var clik2 = document.getElementById('clic2');
-    var clik3 = document.getElementById('clic3');   
-
-    clik1.addEventListener('click',function(){
-        div.style.display='none';
-        count1++;
-    });
-    clik2.addEventListener('click',function(){
-        div.style.display='none';
-        count1++;
-    });
-    clik3.addEventListener('click',function(){
-        div.style.display='none';
-        count1++;    
-    });
+    else if(count===3)
+    {
+        c3.setAttribute('id','content3');
+        cn.innerHTML='';
+        cn.append(c3);
+    }
+    else if(count===4)
+    {
+        c4.setAttribute('id','content4');
+        cn.innerHTML='';
+        cn.append(c4);
+    }
+    else if(count===5)
+    {
+        c5.setAttribute('id','content5');
+        cn.innerHTML='';
+        cn.append(c5);
+    }
+    else if(count===6)
+    {
+        c6.setAttribute('id','content6');
+        cn.innerHTML='';
+        cn.append(c6);
+    }
+    else if(count===7)
+    {
+        c7.setAttribute('id','content7');
+        cn.innerHTML='';
+        cn.append(c7);
+    }
+    else
+    {
+        c8.setAttribute('id','content8');
+        cn.innerHTML='';
+        cn.append(c8);
+    }
 }
 
 
-
+// var a1 = document.getElementById('A');
+// var a2 = document.getElementById('S');
+// var a3 = document.getElementById('E');
